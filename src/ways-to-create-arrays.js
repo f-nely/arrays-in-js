054994af5b2ba518c906b4213d61ef877a9aa9ea
nelynely <nelly.nf54@gmail.com>
2025-07-12 17:50:44 -0300

    Create ways-to-create-arrays.js

---
+++ src/ways-to-create-arrays.js
@@ -0,0 +1,11 @@
+// Array constructor
+const cars = new Array("BYD Dolphin Mini", "Fiat Pulse", "Honda HR-V");
+console.log(cars);
+
+// Array literal notation
+const motorcycles = [
+  "Yamaha YBR 150 Factor",
+  "Honda CB 300F Twister",
+  "Ducati Panigale V4",
+];
+console.log(motorcycles);
