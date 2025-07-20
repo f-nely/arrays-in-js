c49358ef87836d0a2b2311e4beb6ae767bad83cf
nelynely <nelly.nf54@gmail.com>
2025-07-20 15:40:27 -0300

    Create remove-items-to-array.js

---
+++ src/remove-items-to-array.js
@@ -0,0 +1,15 @@
+const amdCpus = [
+  "Intel Core Ultra 9",
+  "Ryzen Z2",
+  "Sempron",
+  "A-Series",
+  "E-Series",
+  "Phenom",
+  "M4",
+];
+
+// console.log(amdCpus);
+amdCpus.shift(); // remove the first element
+// amdCpus.pop();
+// console.log(amdCpus);
+console.log(amdCpus.pop()); // return the removed element
