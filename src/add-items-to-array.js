62ef123dd2317099637cce1bdf7f7eb0dd3c781f
nelynely <nelly.nf54@gmail.com>
2025-07-24 15:33:03 -0300

    Add comments

--- src/add-items-to-array.js
+++ src/add-items-to-array.js
@@ -1,6 +1,6 @@
 const amdCpus = ["Ryzen Z2", "Sempron", "Phenom", "E-Series", "A-Series"];
 
-amdCpus.unshift("Athlon");
-amdCpus.push("PRO A-Series");
+amdCpus.unshift("Athlon"); // insert new elements at the start an array
+amdCpus.push("PRO A-Series"); // insert new elements to the end of an array
 console.log(amdCpus);
 console.log(amdCpus.push("FX-Series")); // return length
