5397e51cfa42dae1d338ee4e2ba57aac42527d7e
nelynely <nelly.nf54@gmail.com>
2025-07-24 15:49:11 -0300

    Create challenge-01.js

---
+++ src/challenge-01.js
@@ -0,0 +1,9 @@
+/* 
+  Neste desafio você irá receber um Array 
+  de números inteiros na constante "numbers".
+  Adicione um número no final do array.
+*/
+
+const numbers = [0, 1, 2, 3];
+
+numbers.push(4);
