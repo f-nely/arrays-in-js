87de1085aba203c037bb59df325f8aee092e7876
nelynely <nelly.nf54@gmail.com>
2025-07-26 15:26:42 -0300

    Create challenge-03.js

---
+++ src/challenge-03.js
@@ -0,0 +1,6 @@
+/**
+ * Remova o elemento que está no índice 7 no Array chamado "numbers".
+ */
+const numbers = [51, 52, 74, 46, 20, 82, 59, 39, 77, 23];
+numbers.splice(7, 1);
+console.log(numbers);
