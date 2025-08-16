9d769fe6d02c1709952ddbf6bfa5ed25aada6bf3
nely de faria <nelly.nf54@gmail.com>
2025-08-16 12:44:50 -0300

    Create reverse.js

---
+++ src/reverse.js
@@ -0,0 +1,3 @@
+const arr1 = ["yes", "no", "maybe", "always", "sometimes", "never", "if"];
+console.log(arr1.reverse()); // [ 'if', 'never', 'sometimes', 'always', 'maybe', 'no', 'yes' ]
+console.log(arr1.sort().reverse());
