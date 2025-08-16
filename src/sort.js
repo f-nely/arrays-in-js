64222caa7692299c4c4fd158f5a365d63f6034cc
nely de faria <nelly.nf54@gmail.com>
2025-08-16 12:44:27 -0300

    Create sort.js

---
+++ src/sort.js
@@ -0,0 +1,8 @@
+const numbers = [5, 6, 3, 1, 4];
+// console.log(numbers.sort());
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.sort()); ["Apple", "Banana", "Mango", "Orange"]
+
+const allArr = [...numbers, ...fruits];
+console.log(allArr.sort());
