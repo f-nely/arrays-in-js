b529f40207953bda52b0f554d028b66e6efdd44b
nely de faria <nelly.nf54@gmail.com>
2025-08-19 14:24:46 -0300

    Create concat.js

---
+++ src/concat.js
@@ -0,0 +1,12 @@
+const cars = ["Civic", "Elantra", "Focus", "Jetta", "Cruze", "Corolla"];
+
+const animals = ["ant", "bison", "camel", "duck", "elephant"];
+
+// const arr1 = ["Indigo", "Teal"].concat(cars).concat(animals);
+// console.log(arr1);
+
+// const arr2 = cars.concat(animals);
+// console.log(arr2);
+
+const arr3 = animals.concat("Le printemps", "L'été");
+console.log(arr3);
